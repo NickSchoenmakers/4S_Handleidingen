4f126e4889252ba03151e841a3b62ebe8512e6dc
NickSchoenmakers <71122447+NickSchoenmakers@users.noreply.github.com>
2021-09-24 14:23:48 +0200

    update
    test

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -3,7 +3,7 @@
     <div class="container">
         <div class="hometext">
             <a href="/" title="{{ __('misc.home_alt') }}" alt="{{ __('misc.home_alt') }}">
-                <h1 style="font-weight: bold">{{ __('misc.homepage_title') }}</h1>
+                <h1 style="font-weight: bold; ">{{ __('misc.homepage_title') }}</h1>
             </a>
             @yield('introduction_text')
         </div>
